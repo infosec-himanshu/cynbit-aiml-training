d518ff48b791722923fa748e0888eb2ee195bdd0
cipher-himanshu <himanshu9de8@gmail.com>
2026-06-15 22:19:32 +0530

    student profile program

---
+++ student.py
@@ -0,0 +1,9 @@
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+city = input("Enter your city: ")
+
+print("\n--- Student Profile ---")
+print(f"Name : {name}")
+print(f"Age  : {age}")
+print(f"City : {city}")
+print("-----------------------")
